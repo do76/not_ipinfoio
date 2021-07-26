02f665e09cad9728c4ddb2186e6f699f5de0b0bc
Dom <30157265+do76@users.noreply.github.com>
2021-07-26 21:16:54 +0300

    Create api.php

---
+++ api.php
@@ -0,0 +1,59 @@
+<?php
+
+/* Check for API Key */
+
+/* Query database (API) */
+
+/* Get IP via PHP, if needed use command line tools */
+
+/* Query RIPE (eu)/APNIC (asia)/AFRNIC (africa)/LANIC (SA)/ARIN (NA) database*/
+
+/* Output */
+
+{
+  "ip": "8.8.8.8",
+  "hostname": "dns.google",
+  "city": "Mountain View",
+  "region": "California",
+  "country": "US",
+  "loc": "37.4056,-122.0775",
+  "postal": "94043",
+  "timezone": "America/Los_Angeles",
+  "asn": {
+    "asn": "AS15169",
+    "name": "Google LLC",
+    "domain": "google.com",
+    "route": "8.8.8.0/24",
+    "type": "business"
+  },
+  "company": {
+    "name": "Google LLC",
+    "domain": "google.com",
+    "type": "business"
+  },
+  "privacy": {
+    "vpn": false,
+    "proxy": false,
+    "tor": false,
+    "hosting": false
+  },
+  "abuse": {
+    "address": "US, CA, Mountain View, 1600 Amphitheatre Parkway, 94043",
+    "country": "US",
+    "email": "network-abuse@google.com",
+    "name": "Abuse",
+    "network": "8.8.8.0/24",
+    "phone": "+1-650-253-0000"
+  },
+  "domains": {
+    "ip": "8.8.8.8",
+    "total": 12872,
+    "domains": [
+      "authrock.com",
+      "itempurl.com",
+      "etempurl.com",
+      "dns.google",
+      "5uuu.net"
+    ]
+  }
+}
